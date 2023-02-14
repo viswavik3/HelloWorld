f2c666f3eda167e8c692f838e3433e9a185c3a67
Viswa <viswavik3@gmail.com>
2023-02-15 02:10:06 +0530

    init

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        assertEquals("Hello, world!", "Hello, world!");
+    }
+}
+
