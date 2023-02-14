c356ffe5b8f94d81c351fb4b53c55bfdb8885371
viswavik3 <123868576+viswavik3@users.noreply.github.com>
2023-02-15 02:34:41 +0530

    Update HelloWorldTest.java

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,14 @@
 package com.example;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
-        assertEquals("Hello, world!", "Hello, world!");
+        HelloWorld helloWorld = new HelloWorld();
+        String expectedOutput = "Hello, world!";
+        String actualOutput = helloWorld.getGreeting();
+        assertEquals(expectedOutput, actualOutput);
     }
 }
-
